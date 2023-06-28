05d46c413be1c5e30b1424ef6699365cd41087ff
arnaud kaderi <arnay07@gmail.com>
2023-06-28 14:03:15 +0200

    added nav bar to home

--- src/main/java/tacos/TacosApplication.java
+++ src/main/java/tacos/TacosApplication.java
@@ -4,21 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-
 @SpringBootApplication()
-@ComponentScan(basePackages = {"tacos", "tacos.web", "tacos.data"})
+@ComponentScan(basePackages = { "tacos", "tacos.web", "tacos.data" })
 public class TacosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TacosApplication.class, args);
 
-        /*
-         * TODO Remove caching line thymeleaf in application.yml for production thymeleaf cache the
-         * results of the template processing, which is great for performance but not so great for
-         * development. If you want to see the changes you make to your templates, you’ll need to
-         * disable the cache. You can do this by setting the thymeleaf cache property to false in
-         * the application.yml file.
-         */
+        // TODO Remove caching line thymeleaf in application.yml for production
+        // thymeleaf cache the
+        // results of the template processing, which is great for performance but not so
+        // great for
+        // development. If you want to see the changes you make to your templates,
+        // you’ll need to
+        // disable the cache. You can do this by setting the thymeleaf cache property to
+        // false in
+        // the application.yml file.
+
     }
 
 }
